9c0dacec3d9b7821c382984ce6d27a468048781d
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2021-10-29 23:51:04 -0400

    Implement custom global command events

---
+++ src/main/java/me/bdx/managerapi/customEvents/onGlobalCommandReceive.java
@@ -0,0 +1,47 @@
+package me.bdx.managerapi.customEvents;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class onGlobalCommandReceive extends Event implements Cancellable {
+    private static final HandlerList HANDLERS = new HandlerList();
+    private boolean isCancelled = false;
+    private String command;
+    private String commandSenderName;
+
+
+    public onGlobalCommandReceive(String command_string, String sender){
+        super(true);
+        this.command = command_string;
+        this.commandSenderName = sender;
+        this.isCancelled = false;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean isCancelled) {
+        this.isCancelled = isCancelled;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    public String getCommand(){
+        return this.command;
+    }
+
+    public String getCommandSenderName(){
+        return this.commandSenderName;
+    }
+}
