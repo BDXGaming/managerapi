31c20b969af826785b002d08cad4c9846e811ca8
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2021-10-30 00:16:37 -0400

    Add chatsender class

---
+++ src/main/java/me/bdx/managerapi/chat/ChatSender.java
@@ -0,0 +1,38 @@
+package me.bdx.managerapi.chat;
+
+import me.bdx.managerapi.api.chatApi;
+import org.bukkit.ChatColor;
+
+public class ChatSender {
+
+    private String Name;
+    private String DisplayName;
+    private String chatcolor;
+
+    public ChatSender(String name, String DisplayName){
+        this.Name = name;
+        this.DisplayName = DisplayName;
+        this.chatcolor = "white";
+    }
+
+    public ChatSender(String name){
+        this.DisplayName = name;
+        this.Name = name;
+        this.chatcolor = "white";
+    }
+
+    public ChatSender(String name, String DisplayName, String color){
+        this.Name = name;
+        this.DisplayName = DisplayName;
+        this.chatcolor = color;
+    }
+
+    public void setChatColor(String chatColor){
+        this.chatcolor = chatColor;
+    }
+
+    public void sendGlobalMessage(String message) throws Exception {
+        chatApi.sendCustomMsg(this.Name, this.DisplayName, message, "chat-g", "g", chatcolor);
+    }
+
+}
