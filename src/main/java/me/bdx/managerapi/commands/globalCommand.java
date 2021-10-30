2241b90d140535814d0e180f9340033fdfe04a73
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2021-10-29 23:51:14 -0400

    Implement custom global command events

--- src/main/java/me/bdx/managerapi/commands/globalCommand.java
+++ src/main/java/me/bdx/managerapi/commands/globalCommand.java
@@ -2,6 +2,7 @@ package me.bdx.managerapi.commands;
 
 import me.bdx.managerapi.Managerapi;
 import me.bdx.managerapi.api.chatApi;
+import me.bdx.managerapi.customEvents.onGlobalCommandEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -15,18 +16,26 @@ public class globalCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if(sender.hasPermission("managerapi.globalcommand")){
+
             StringBuilder command = new StringBuilder();
             if(args.length > 0){
                 for(String word: args){
                     command.append(word).append(" ");
                 }
-                Managerapi.commandHandler.processCommand(command.toString());
-                try {
-                    chatApi.sendGlobalCommand(command.toString(), sender.getName());
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                onGlobalCommandEvent event = new onGlobalCommandEvent(command.toString(), sender.getName());
+                Bukkit.getServer().getPluginManager().callEvent(event);
+
+                if(!event.isCancelled()){
+                    Managerapi.commandHandler.processCommand(command.toString());
+                    try {
+                        chatApi.sendGlobalCommand(command.toString(), sender.getName());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Bukkit.broadcast( ChatColor.GRAY + "[GC Log] "+sender.getName()+ " used the command /"+command.toString(), "managerapi.globalcommand.notify");
+                    return true;
                 }
-                Bukkit.broadcast( ChatColor.GRAY + "[GC Log] "+sender.getName()+ " used the command /"+command.toString(), "managerapi.globalcommand.notify");
+                sender.sendMessage(ChatColor.RED + "GlobalCommand has been cancelled");
                 return true;
             }
             sender.sendMessage(ChatColor.RED + "You need to enter a command to use!");
