354a800ce667097921a5afda1462a7569cba5f05
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2021-11-23 18:29:44 -0500

    Add custom event framework

---
+++ src/main/java/me/bdx/managerapi/customEvents/staffChatReceiveEvent.java
@@ -0,0 +1,29 @@
+package me.bdx.managerapi.customEvents;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class staffChatReceiveEvent extends Event implements Cancellable {
+    private static final HandlerList HANDLERS = new HandlerList();
+    private boolean isCancelled = false;
+
+    @Override
+    public boolean isCancelled() {
+        return this.isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean isCancelled) {
+        this.isCancelled = isCancelled;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+}
---
+++ src/main/java/me/bdx/managerapi/globalData/globalPlayers.java
@@ -0,0 +1,54 @@
+package me.bdx.managerapi.globalData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class globalPlayers {
+
+    private ArrayList<String> onlinePlayers;
+    private HashMap<String, String> onlinePlayerServers;
+
+    /**
+     * Creates a new instance of the globalPlayers class
+     * Intilizes a new Arraylist for the online players
+     */
+    public globalPlayers(){
+        this.onlinePlayers = new ArrayList<String>();
+        this.onlinePlayerServers = new HashMap<String, String>();
+    }
+
+    /**
+     * Sets the onlinePlayers Arraylist to the arraylist provided
+     * @param players Arraylist<String>
+     */
+    public void setOnlinePlayers(ArrayList<String> players){
+        this.onlinePlayers = players;
+    }
+
+    /**
+     * Gets the arraylist of onlinePlayers
+     * This is the list of all online players
+     * @return Arraylist<String>
+     */
+    public ArrayList<String> getOnlinePlayers() {
+        return onlinePlayers;
+    }
+
+    /**
+     * Gets the HashMap of all onlinePlayerServers
+     * Data format HashMap<String username>, HashMap<String server, UUID uuid>
+     * @return HashMap<String, HashMap<String, UUID>>
+     */
+    public HashMap<String, String> getOnlinePlayerServers() {
+        return onlinePlayerServers;
+    }
+
+    /**
+     * Sets the HashMap of onlinePlayerList to given value
+     * @param dict HashMap<String, HashMap<String, UUID>>
+     */
+    public void setOnlinePlayerServers(HashMap<String, String> dict){
+        this.onlinePlayerServers = dict;
+    }
+}
